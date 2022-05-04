09cd55dea6a763749a3c51a84ea47053a3925e0e
Shilpa KL <Shilpa KL@LAPTOP-KSCNGTRR>
2022-05-04 23:52:14 +0530

    First Commit

---
+++ Day 21_Annotations/src/annotations/Example4.java
@@ -0,0 +1,34 @@
+package annotations;
+import java.lang.reflect.Method;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+@interface MyAnnotation
+{
+	 
+
+	int print();
+	
+}
+
+class Parent2
+{
+	 @MyAnnotation(print=23)
+	 public void display()
+	 {
+		 System.out.println("Welcome to Annotations");
+	 }
+}
+
+ class Example4
+ {
+	 public static void main(String args[]) throws Exception
+	 {
+		 Parent2 p=new Parent2();
+		 Method m=p.getClass().getMethod("display");
+		 MyAnnotation a=m.getAnnotation(MyAnnotation.class);
+		 System.out.println(a.print());
+	 }
+ }
