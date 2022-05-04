5e21f92a54937918018f316b46cedb067476d9a7
Shilpa KL <Shilpa KL@LAPTOP-KSCNGTRR>
2022-05-04 23:45:55 +0530

    first Commit

---
+++ Day 14_Strings/src/string/Example3.java
@@ -0,0 +1,19 @@
+package string;
+
+public class Example3 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		//String str="Shilpa";
+		//String str1="Shilpa";
+		String str1=new String("Shilpa");
+		String str2=new String("Shilpa");
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));
+		
+	
+	}
+
+}
