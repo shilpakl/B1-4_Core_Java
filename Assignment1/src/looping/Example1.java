a13f15f1ec7ff66361e7979f306009c593a8b848
Shilpa KL <Shilpa KL@LAPTOP-KSCNGTRR>
2022-05-04 21:46:05 +0530

    First Commit

---
+++ Assignment1/src/looping/Example1.java
@@ -0,0 +1,19 @@
+package looping;
+
+import java.util.Scanner;
+
+public class Example1 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();//10
+		for(int i=1;i<=n;i++)
+		{
+			System.out.print(i+" ");
+			s.close();
+		}
+	}
+
+}
---
+++ Assignment1/src/operator/TernoryOperator.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class TernoryOperator 
+{
+
+	public static void main(String[] args) 
+	{
+
+		int a=5,b=2;
+		int res=(a%b==0)? a:b;
+		System.out.println(res);
+
+
+	}
+
+}
